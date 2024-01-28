5b60bf8ac2e3462940c0f6849bf4976ae32349aa
watur <waturandjordy@gmail.com>
2024-01-28 23:39:34 +0800

    Exercise 2

---
+++ members.js
@@ -0,0 +1,10 @@
+const members = [
+    {
+      id: 2200604,
+      nim: "105022010090",
+      nama: "Waturandang, Jordy Alfa Elroy",
+      nomorTelp: "082296779901",
+    },    
+  ];
+  
+  module.exports = members;
