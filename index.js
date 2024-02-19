d3cc4a7bc951b9f67f393b783ea458e669d6cff0
Jordy <107411562+JordyWaturandang@users.noreply.github.com>
2024-02-19 21:57:31 +0800

    exercise 3 dan 4

--- index.js
+++ index.js
@@ -1,47 +1,57 @@
-// index.js
-const http = require("http");
-const moment = require("moment");
-const members = require("./members");
-const usersData = require('./users');
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  const url = req.url;
-
-  if (url === "/") {
-    res.write("This is the home page");
-
-  } else if (url === "/about") {
-    const currentDate = moment().format();
-    const responseData = {
-      Status: "success",
-      Message: "response success",
-      Description: "Exercise #03",
-      Date: currentDate,
-      Data: members,
-    };
-    res.write(JSON.stringify(responseData, null, 2));
-
-  } else if (url === "/users") {
-    const responseData = {
-        Status: "success",
-        Message: "response success",
-        Description: "User Data",
-        Data: usersData,
-      };
-      res.write(JSON.stringify(responseData, null, 2));
-  } else {
-    res.write("Invalid URL");
-  }
-
-  res.end();
-});
-
-const hostname = "127.0.0.1";
+const express = require("express");
+const app = express();
+const users = require("./users.js");
 const port = 3000;
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+const morgan = require("morgan")
+const moment = require("moment");
+
+
+const log = (req, res, next) => {
+    console.log('${moment().format("LLLL")} = ${req.ip} = ${req.originalUrl}');
+  next();
+};
+app.use(log);
+app.use(morgan("combined"));
+
+app.get('/', (req, res) => res.write("this is home page"))
+
+app.get("/users", (req, res) => {
+  res.json(users);
 });
 
-console.log(usersData);
+app.get("/users/:name", (req, res) => {
+  const result = users.filter((value) => {
+    return (
+      value.name.toLocaleLowerCase() === req.params.name.toLocaleLowerCase()
+    );
+  });
+  if (result == "") {
+    res.json({
+      message: "Data user tidak ditemukan",
+    });
+  } else {
+    res.json(result);
+  }
+});
+
+const errorHandling = (err, req, res, next) => {
+  res.json({
+    status: "Error",
+    message: "Terjadi kesalahan pada server",
+  });
+};
+
+
+const notFound = (req, res, next) => {
+  res.json({
+    status: "Error",
+    message: "Resource tidak ditemukan",
+  });
+};
+
+app.use(errorHandling);
+app.use(notFound);
+
+app.listen(port, () =>
+  console.log('Server running at http://localhost:${port}')
+);
